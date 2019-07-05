a4dbe4edc73fccab6cf5fdb561be91fdb346f04b
mrtdmr <muratdemir.pau@gmail.com>
2019-07-05 17:00:51 +0300

    05.07.2019_17:00

--- BookList/Models/ErrorViewModel.cs
+++ BookList/Models/ErrorViewModel.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace BookList.Models
 {
     public class ErrorViewModel
--- ProductMicroservice/Controllers/ProductController.cs
+++ ProductMicroservice/Controllers/ProductController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Transactions;
-using Microsoft.AspNetCore.Http;
+﻿using System.Transactions;
 using Microsoft.AspNetCore.Mvc;
 using ProductMicroservice.Model;
 using ProductMicroservice.Repository;
