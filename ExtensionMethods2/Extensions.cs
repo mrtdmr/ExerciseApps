9d95bc8f639a989dbaa5d8af2e24160d3d5760e2
mrtdmr <muratdemir.pau@gmail.com>
2019-07-16 19:39:30 +0300

    16.07.2019

---
+++ ExtensionMethods2/Extensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace ExtensionMethods2
+{
+    public static class Extensions
+    {
+        public static void Sort(this int[] array) // extension metodlar sadece static sınıflar içinde tanımlanabilir. Bu örnekte sadece int tipinde arraylerde çalışır.
+        {
+            int temp = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        temp = array[j + 1];
+                        array[j + 1] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+        }
+        public static void Sort(this int[] array, bool reverse = false) // extension metodlar sadece static sınıflar içinde tanımlanabilir. Bu örnekte sadece int tipinde arraylerde çalışır.
+        {
+            array.Sort();
+            if (reverse)
+            {
+                Array.Reverse(array);
+            }
+        }
+        public static void Reverse(this int[] array)
+        {
+            Array.Reverse(array);
+        }
+        public static Distance DistanceTo(this Point p1, Point p2)
+        {
+            Console.WriteLine($"Teh distance between P1 and P2 is:" +
+                $"\n{p2.X - p1.X} units in the X direction" +
+                $"\n{p2.Y - p1.Y} units in the Y direction");
+
+            return new Distance() { XDistance = p2.X - p1.X, YDistance = p2.Y - p1.Y };
+        }
+    }
+}
---
+++ ExtensionMethods2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Drawing;
+
+namespace ExtensionMethods2
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] array = { 5,3,2,4,8,10,45,84,7,1};
+            //array.Reverse();
+            //Console.WriteLine(string.Join(", ", array));
+            //array.Sort(true);
+            //Console.WriteLine(string.Join(", ",array));
+            Point pointOne = new Point(20,30);
+            Point pointTwo = new Point(10, 15);
+            pointOne.DistanceTo(pointTwo);
+        }
+    }
+    public class Distance
+    {
+        public int XDistance { get; set; }
+        public int YDistance { get; set; }
+    }
+}
