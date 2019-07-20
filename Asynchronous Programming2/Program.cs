8a0d3422fd4c7e3cca5d7a45d85324cfd0cb60e4
murat <muratdemir.pau@gmail.com>
2019-07-20 14:49:19 +0300

    20.07.2019_14:49

---
+++ Asynchronous Programming2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsynchronousProgramming2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            //Task t = new Task(ConcatenateChars);
+            //t.Start();
+            int count = 200000;
+            char charToConcatenate = '1';
+            Task<string> t = Task.Factory.StartNew(() => ConcatenateChars(charToConcatenate, count));
+            Console.WriteLine("In Progress..");
+            t.Wait();
+            Console.WriteLine("Completed");
+            Console.WriteLine("The length of the result is "+t.Result.Length);
+            */
+
+            int count = 200000;
+            char charToConcatenate = '1';
+            Task<string> t = ConcatenateCharsAsync(charToConcatenate, count);
+            Console.WriteLine("In Progress..");
+            Console.WriteLine("The length of the result is " + t.Result.Length);
+        }
+        /*
+        public static string ConcatenateChars(char charToConcatenate, int count)
+        {
+            string concatenatedString = string.Empty;
+            for (int i = 0; i < count; i++)
+            {
+                concatenatedString += charToConcatenate;
+            }
+            return concatenatedString;
+        }
+        */
+        public async static Task<string> ConcatenateCharsAsync(char charToConcatenate, int count)
+        {
+            return await Task<string>.Factory.StartNew(() => {
+                return ConcatenateChars(charToConcatenate, count);
+            });
+        }
+        public static string ConcatenateChars(char charToConcatenate, int count)
+        {
+            string concatenatedString = string.Empty;
+            for (int i = 0; i < count; i++)
+            {
+                concatenatedString += charToConcatenate;
+            }
+            return concatenatedString;
+        }
+    }
+}
